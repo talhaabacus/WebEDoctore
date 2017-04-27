3baed7c99dda2a30861f94ae9d84182f749c5694
AbacusSoftAneel <aneel.lalwani09cs@hotmail.com>
2017-04-27 20:36:27 +0500

    Add

---
+++ WebEDoc2017/WebEdoc2017/WebEdoc2017.DataBaseAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebEdoc2017.DataBaseAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebEDoc2017/WebEdoc2017/WebEdoc2017.Model/DocumentCategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebEdoc2017.Models
+{
+   public class DocumentCategoryModel
+    {
+        public Int64 DOC_CATEGORY_ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Int64 Parent_ID { get; set; }
+    }
+}
