da431937347fe16d974e0899f1dc65c2e637c06e
Talha Shaikh <tshaikh@abacusoft.com>
2017-05-02 22:15:01 +0500

    TConnect.cs added

---
+++ WebEDoc2017/WebEdoc2017/WebEdoc2017.TrustedConnection/TConnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebEdoc2017.TrustedConnection
+{
+    public class TConnect
+    {
+        TConnect()
+        {
+        }
+
+
+    }
+}
