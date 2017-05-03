a86e694b6b45109681880d051dcaddb48c5ffba2
Talha Shaikh <tshaikh@abacusoft.com>
2017-05-04 00:59:20 +0500

    Trusted connection lib and service checked in

--- WebEDoc2017/WebEdoc2017/WebEdoc.Service/TCService.asmx.cs
+++ WebEDoc2017/WebEdoc2017/WebEdoc.Service/TCService.asmx.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using TC = WebEdoc2017.TrustedConnection;
+using System.Web.Script.Services;
 
 namespace WebEdoc.Service
 {
     /// <summary>
     /// Summary description for TCService
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+
+    [WebService(Namespace = "TCService")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
+    [ScriptService]
+
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class TCService : System.Web.Services.WebService
     {
+        [WebMethod]
+        public string EncryptAES(string TextToEncrypt, string EncryptionKey)
+        {
+            return TC.TConnect.EncryptAES(TextToEncrypt, EncryptionKey);
+        }
 
         [WebMethod]
-        public string HelloWorld()
+        public string DecryptAES(string TextToDecrypt, string EncryptionKey)
         {
-            return "Hello World";
+            return TC.TConnect.DecryptAES(TextToDecrypt, EncryptionKey);
+        }
+
+        [WebMethod]
+        public string CalculateSHA2Hash(string InputString)
+        {
+            return TC.TConnect.CalculateSHA2Hash(InputString);
         }
     }
 }
--- WebEDoc2017/WebEdoc2017/WebEdoc2017.TrustedConnection/TConnect.cs
+++ WebEDoc2017/WebEdoc2017/WebEdoc2017.TrustedConnection/TConnect.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace WebEdoc2017.TrustedConnection
 {
@@ -12,6 +14,67 @@ namespace WebEdoc2017.TrustedConnection
         {
         }
 
+        public static string EncryptAES(string TextToEncrypt, string EncryptionKey)
+        {
+            string Encryption_Key = EncryptionKey;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(TextToEncrypt);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    TextToEncrypt = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return TextToEncrypt;
+        }
+
+        public static string DecryptAES(string TextToDecrypt, string EncryptionKey)
+        {
+            string Encryption_Key = EncryptionKey;
+            byte[] cipherBytes = Convert.FromBase64String(TextToDecrypt);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    TextToDecrypt = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return TextToDecrypt;
+        }
+
+        public static string CalculateSHA2Hash(string InputString)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(InputString);
+            byte[] hash = sha256.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
 
     }
 }
